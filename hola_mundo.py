63c3441f0a44b6ea3815b813b3ae6cf51e16b322
isabela montaño <isabelamontanobedoya@gmail.com>
2026-07-21 10:46:56 -0500

    hola46

---
+++ hola_mundo.py
@@ -0,0 +1,8 @@
+#mi script de primer.py
+ Imprimir( "hola, Github" )
+ Imprimir( "hola, Github" )
+ Imprimir( "hola, Github" )
+ Imprimir( "ciao, Github" )
+ Imprimir( "hola, Github" )
+ Imprimir( "hola, Github" )
+ Imprimir( "nihao, Github" )
